ea7fba1f3a716a63a5e59be7214a67137248718b
What-ever-this-is <yuchestart@gmail.com>
2023-03-23 20:24:15 -0500

    check

--- pwmtest.py
+++ pwmtest.py
@@ -9,6 +9,7 @@ g.setup(IN1,g.OUT)
 g.setup(IN2,g.OUT)
 pwm = g.PWM(ENA,1000)
 pwm.start(25)
+speed = 25
 g.output(IN1,g.LOW)
 g.output(IN2,g.LOW)
 
@@ -16,15 +17,17 @@ while True:
     command = input().split()
     if(command[0] == "o"):
         pwm.ChangeDutyCycle(int(command[1]))
+        speed = 25
     elif(command[0] == "f"):
+        pwm.ChangeDutyCycle(speed)
         g.output(IN1,g.HIGH)
         g.output(IN2,g.LOW)
     elif(command[0] == "b"):
+        pwm.ChangeDutyCycle(speed)
         g.output(IN2,g.HIGH)
         g.output(IN1,g.LOW)
     elif(command[0] == "h"):
-        g.output(IN2,g.LOW)
-        g.output(IN1,g.LOW)
+        pwm.ChangeDutyCycle(0)
     elif(command[0] == "e"):
         g.cleanup()
         exit()
