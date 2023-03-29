4506c46d680a6238b0b71fa96fefd1d41809350f
What-ever-this-is <yuchestart@gmail.com>
2023-03-28 20:50:51 -0500

    DONE!

--- finaltest.py
+++ finaltest.py
@@ -1,99 +1,94 @@
-# FINAL REVISION BEFORE PORT TO ROS
 import RPi.GPIO as g
 import time as t
-class Driver:
-    def __init__(self,v1,v2,in1,in2,in3,in4,e1a,e1b,e2a,e2b):
-        g.setmode(g.BOARD)
-        g.setup(v1,g.OUT)
-        g.setup(v2,g.OUT)
-        g.setup(in1,g.OUT)
-        g.setup(in2,g.OUT)
-        g.setup(in3,g.OUT)
-        g.setup(in4,g.OUT)
-        g.setup(e1a,g.IN)
-        g.setup(e1b,g.IN)
-        g.setup(e2a,g.IN)
-        g.setup(e2b,g.IN)
-        g.add_event_detect(e1a,g.RISING,callback=self.readEncoderA)
-        g.add_event_detect(e2a,g.RISING,callback=self.readEncoderB)
-        self.pins = {
-            "v1":v1,
-            "v2":v2,
-            "in1":in1,
-            "in2":in2,
-            "in3":in3,
-            "in4":in4,
-            "e1a":e1a,
-            "e1b":e1b,
-            "e2a":e2a,
-            "e2b":e2b
-        }
-        self.pwm = {
-            "a":g.PWM(v1,1000),
-            "b":g.PWM(v2,1000)
-        }
-        self.speed = {
-            "a":0,
-            "b":0
-        }
-        self.encoderPositions = {
-            "a":0,
-            "b":0
-        }
-        self.lastTimePerEncoder = {
-            "a":t.time(),
-            "b":t.time()
-        }
-        self.encoderSpeeds = {
-            "a":0,
-            "b":0
-        }
-        self.running = False
-        self.pwm["a"].start(0)
-        self.pwm["b"].start(0)
-    def setMotor(self,speedA,speedB):
-        requested = {
-            "a":speedA,
-            "b":speedB
-        }
-        for motor in range(self.speed):
-            self.speed = requested[motor]
-            self.pwm[motor].ChangeDutyCycle(abs(requested[motor]))
-            if requested[motor] < 0:
-                if motor == "a":
-                    g.output(self.pins['in1'],g.LOW)
-                    g.output(self.pins['in2'],g.HIGH)
-                else:
-                    g.output(self.pins['in3'],g.LOW)
-                    g.output(self.pins['in4'],g.HIGH)
+def readEncoderA(x):
+    global A,B,E1B,speeds,times
+    e = g.input(E1B)
+    if e:
+        A+=1
+    else:
+        A-=1
+    speeds["A"] = round(1/(t.time()-times["A"])*100)/100
+    times["A"] = t.time()
+def readEncoderB(x):
+    global A,B,E2B,speeds,times
+    e = g.input(E2B)
+    if e:
+        B+=1
+    else:
+        B-=1
+    speeds["B"] = round(1/(t.time()-times["B"])*100)/100
+    times["B"] = t.time()
+def pidLoop():
+    global VA,VB,IN1,IN2,IN3,IN4,setpoints,speeds,pwms,prevT,KP,KI,INTEGRAL
+    for motor in ["A","B"]:
+        e = setpoints[motor]-speeds[motor]
+        newtime = t.time()
+        deltaTime = newtime - prevT
+        prevT = newtime
+        INTEGRAL = INTEGRAL + e*deltaTime
+        u = KP*e + KI*INTEGRAL
+        if(u<0):
+            if motor == "A":
+                g.output(IN1,g.LOW)
+                g.output(IN2,g.HIGH)
             else:
-                if motor == "a":
-                    g.output(self.pins['in2'],g.LOW)
-                    g.output(self.pins['in1'],g.HIGH)
-                else:
-                    g.output(self.pins['in4'],g.LOW)
-                    g.output(self.pins['in3'],g.HIGH)
-    def readEncoderA(self,n=None):
-        encoderBValue = g.input(self.pins["e1b"])
-        if encoderBValue:
-            self.encoderPositions["a"]+=1
+                g.output(IN3,g.LOW)
+                g.output(IN4,g.HIGH)
         else:
-            self.encoderPositions["a"]-=1
-        self.encoderSpeeds["a"] = 1/self.lastTimePerEncoder["a"]
-        self.lastTimePerEncoder["a"] = t.time()
-    def readEncoderB(self,n=None):
-        encoderBValue = g.input(self.pins["e2b"])
-        if encoderBValue:
-            self.encoderPositions["b"]+=1
-        else:
-            self.encoderPositions["b"]-=1
-        self.encoderSpeeds["b"] = 1/self.lastTimePerEncoder["a"]
-        self.lastTimePerEncoder["b"] = t.time()
-    def pidLoop(self):
-        velA = self.encoderSpeeds["a"]/373 # Revolutions per second
-        velB = self.encoderSpeeds["b"]/373
-        print(velA,velB)
-        t.sleep()
-        if self.running:
-            self.pidLoop() 
-    
+            if motor == "A":
+                g.output(IN2,g.LOW)
+                g.output(IN1,g.HIGH)
+            else:
+                g.output(IN4,g.LOW)
+                g.output(IN3,g.HIGH)
+        pwr = int(abs(u))
+        if pwr > 100:
+            pwr = 100
+        pwms[motor].ChangeDutyCycle(pwr)
+INTEGRAL = 0  
+prevT = t.time()
+A = 0
+B = 0
+KP = 0.5
+KI = 0.1
+E1A = 18
+E1B = 22
+E2A = 29
+E2B = 31
+VA = 7
+VB = 11
+IN1 = 12
+IN2 = 13
+IN3 = 15
+IN4 = 16
+g.setup(E1A,g.IN)
+g.setup(E1B,g.IN)
+g.setup(E2A,g.IN)
+g.setup(E2B,g.IN)
+g.setup(VA,g.OUT)
+g.setup(VB,g.OUT)
+g.setup(IN1,g.OUT)
+g.setup(IN2,g.OUT)
+g.setup(IN3,g.OUT)
+g.setup(IN4,g.OUT)
+setpoints ={
+    "A":0,
+    "B":0
+}
+
+times = {
+    "A":t.time(),
+    "B":t.time()
+}
+speeds = {
+    "A":0,
+    "B":0
+}
+pwms = {
+    "A":g.PWM(VA,1000),
+    "B":g.PWM(VB,1000)
+}
+pwms["A"].start(0)
+pwms["B"].start(0)
+g.add_event_detect(E1A,g.RISING,callback=readEncoderA)
+g.add_event_detect(E2A,g.RISING,callback=readEncoderB)
